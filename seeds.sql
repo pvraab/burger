b957f789a65ac81150dc0881ae82f11cfa6e757c
Paul Raab <pvraab@hotmail.com>
2019-06-21 09:56:51 -0600

    First pass

---
+++ seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chesseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Hamburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Mushroom Chesseburger', false);
